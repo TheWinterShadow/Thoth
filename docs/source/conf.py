43a302df4ac9817ce780d89ca519f493d45f7a2c
TheWinterShadow <elijah.j.winter@outlook.com>
2025-12-30 19:34:23 -0500

    Add Sphinx documentation configuration and initial files; update project scripts
    Signed-off-by: TheWinterShadow <elijah.j.winter@outlook.com>

---
+++ docs/source/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# set of options see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
+# -- Project information -----------------------------------------------------
+
+project = 'Thoth'
+copyright = '2025 TheWinterShadow'
+author = 'TheWinterShadow'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
